49abec230a533805984ec14548858432d678ff76
tognia <h.tognia@ziyouma-agency.com>
2026-03-10 12:23:41 +0100

    some updates

---
+++ calculatornew/operations.py
@@ -0,0 +1,14 @@
+def sum(a, b):
+    return a + b
+
+def substraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        print("Erreur : division par zéro")
--- main.py
+++ main.py
@@ -184,61 +184,73 @@
 # salaire_horaire = salaire_horaire(salaire_hebdomadaire, heures_travaillees)
 # print(f"Votre salaire horaire est de : {salaire_horaire:.2f}")
 
-def print_welcome_message():
-    print("Bienvenue sur la mini-calculatrice !")
+# def print_welcome_message():
+#     print("Bienvenue sur la mini-calculatrice !")
     
-def input_two_number():
-    num1 = float(input("Entrez le premier nombre : "))
-    num2 = float(input("Entrez le deuxième nombre : "))
-    return num1, num2
+# def input_two_number():
+#     num1 = float(input("Entrez le premier nombre : "))
+#     num2 = float(input("Entrez le deuxième nombre : "))
+#     return num1, num2
 
-def print_menu_and_get_choice():
-    print("=== MENU ===")
-    print("1. Addition")
-    print("2. Soustraction")
-    print("3. Multiplication")
-    print("4. Division")
+# def print_menu_and_get_choice():
+#     print("=== MENU ===")
+#     print("1. Addition")
+#     print("2. Soustraction")
+#     print("3. Multiplication")
+#     print("4. Division")
 
-    user_choice = input("Entrez votre choix (1-4) : ")
+#     user_choice = input("Entrez votre choix (1-4) : ")
 
-    while user_choice not in ["1", "2", "3", "4"]:
+#     while user_choice not in ["1", "2", "3", "4"]:
 
-        user_choice = input("Choix invalide. Entrez votre choix (1-4) : ")
+#         user_choice = input("Choix invalide. Entrez votre choix (1-4) : ")
 
-    return user_choice
+#     return user_choice
 
-def sum(a, b):
-    return a + b
+# def sum(a, b):
+#     return a + b
 
-def substraction(a, b):
-    return a - b
+# def substraction(a, b):
+#     return a - b
 
-def multiplication(a, b):
-    return a * b
+# def multiplication(a, b):
+#     return a * b
 
-def division(a, b):
-    if b != 0:
-        return a / b
-    else:
-        print("Erreur : division par zéro")
+# def division(a, b):
+#     if b != 0:
+#         return a / b
+#     else:
+#         print("Erreur : division par zéro")
 
-def run_calculation(user_choice):
-    num1, num2 = input_two_number()
-    match user_choice:
-        case '1':
-            result = sum(num1, num2)
-        case '2':
-            result = substraction(num1, num2)
-        case '3':
-            result = multiplication(num1, num2)
-        case '4':
-            result = division(num1, num2)
-        case _:
-            print("Choix invalide.")
-    return result
+# def run_calculation(user_choice):
+#     num1, num2 = input_two_number()
+#     match user_choice:
+#         case '1':
+#             result = sum(num1, num2)
+#         case '2':
+#             result = substraction(num1, num2)
+#         case '3':
+#             result = multiplication(num1, num2)
+#         case '4':
+#             result = division(num1, num2)
+#         case _:
+#             print("Choix invalide.")
+#     return result
 
-if __name__ == '__main__':
-    print_welcome_message()
-    user_choice = print_menu_and_get_choice()
-    result = run_calculation(user_choice)
-    print(result)
+# if __name__ == '__main__':
+#     print_welcome_message()
+#     user_choice = print_menu_and_get_choice()
+#     result = run_calculation(user_choice)
+#     print(result)
+
+
+from calculatornew.operations import sum, division
+import requests
+
+if __name__ == "__main__":
+    resultat = sum(20, 30)
+    resultat2 = division(30,2)
+    print(resultat, resultat2)
+
+    resultat3 = requests.get("https://google.com")
+    print(resultat3.content)
